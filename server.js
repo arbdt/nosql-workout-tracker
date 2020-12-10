de688994699c95134313da5d486a34dae4a2645f
arbdt <67365149+arbdt@users.noreply.github.com>
2020-12-10 23:56:20 +1030

    added app.use() to server for JSON, URL parsing, and router module

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
+/*--------*\
+|  Server  |
+\*--------*/
+
 // import modules
+const { json } = require("express");
 let express = require("express");
 let mongoose = require("mongoose");
 
 // set up server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // PORT
 let app = express();
+app.use(express.urlencoded({extended: true})); // URL params
+app.use(express.json()); // JSON format
+app.use(require("./routes/router")); // router module
 
 // set up mongoose
 mongoose.connect("mongodb://localhost/workout", {

05136e5715b30e2f744d57399c0bee7e7822e4ed
arbdt <67365149+arbdt@users.noreply.github.com>
2020-12-12 19:57:03 +1030

    changed format of add exercise and  get latest workout routes, and added home page route

--- routes/router.js
+++ routes/router.js
@@ -6,12 +6,13 @@
 const router = require("express").Router();
 const Exercise = require("../models/exerciseModel");
 const Workout = require("../models/workoutModel");
+const path = require("path");
 
 // api.js ----
 // get last workout
 // GET `/api/workouts`
 router.get(`/api/workouts`, function (request, response){
-    Workout.find({}).then(function(result){
+    Workout.find({}).sort({ createdAt: -1}).limit(1).then(function(result){
         response.json(result);
     });
 });
@@ -20,7 +21,7 @@ router.get(`/api/workouts`, function (request, response){
 // PUT `/api/workouts/id`
 router.put(`/api/workouts/:id`, function (request, response){
     // create exercise from request data
-    let newExercise = await Exercise.create({
+    let newExercise = Exercise.create({
         type: request.body.type,
         name: request.body.name,
         distance: request.body.distance,
@@ -28,13 +29,9 @@ router.put(`/api/workouts/:id`, function (request, response){
         weight: request.body.weight,
         reps: request.body.reps,
         sets: request.body.sets
-    });
-
-    // get matching workout
-    let workoutId = request.params.id;
-
-    // update workout by appending new exercise
-    let updatedWorkout = await Workout.findOneAndUpdate({"_id": workoutId}, {$push: {exercises: newExercise}}, {new: true});
+    }).next(
+        // add to workout with ID from parameter
+        Workout.findOneAndUpdate({"_id": request.params.id}, {$push: {exercises: newExercise}}, {new: true}));
 
     // return updated exercise
     response.json(updatedWorkout);
@@ -50,6 +47,11 @@ router.post(`/api/workouts`, function (request, response){
 // GET `/api/workouts/range`
 router.get(`/api/workouts/range`, function (request, response){
 
+});
+
+// home page
+router.get("/", function (request, response){
+    response.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // stats.js ----
